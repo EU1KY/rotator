4aa99538baf068f858280268b5fea0d500a1235e
EU1KY <kuchura@gmail.com>
2024-03-18 15:06:03 +0300

    Updated initial py upload sequence

--- upload_initial.bat
+++ upload_initial.bat
@@ -1,9 +1,9 @@
-set AMPY_PORT=COM14
+set AMPY_PORT=COM4
+ampy put boot.py
 ampy put templates
 ampy put static
 ampy put lib
-ampy put main.py
-ampy put config.py
 ampy put uftpd.py
-ampy put boot.py
+ampy put config.py
+ampy put main.py
 pause
